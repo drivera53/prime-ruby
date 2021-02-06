fe24a075490ef58d7ea70396222941fe3e649f82
Danny Rivera <samboard@hotmail.com>
2021-02-06 13:03:02 -0500

    commit tree 4359855a992288f00c135cabe436feae89d5e9d2

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
+require 'pry'
 # Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+    else
+        iteration = 2
+        while iteration < number
+            return false if number % iteration == 0
+            iteration += 1
+        end
+        return true
+    end
+end
+
+#binding.pry
